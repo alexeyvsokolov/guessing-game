41b52209d7c341806a273f245a75a62c007bed5f
alexeyvsokolov <alexeysokolovgeneral@gmail.com>
2022-06-03 15:31:03 +0300

    fix and style code

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
 let maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
 alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
-let answerNumber  = Math.floor((minValue + maxValue) / 2);
+let answerNumber = Math.floor((minValue + maxValue) / 2); // Вопрос: "Вы загадали число " "?" - Середина числового диапазона
 let orderNumber = 1;
 let gameRun = true;
 
-const orderNumberField = document.getElementById('orderNumberField');
+const orderNumberField = document.getElementById('orderNumberField'); // Вопрос №_
 const answerField = document.getElementById('answerField');
 
 orderNumberField.innerText = orderNumber;
-answerField.innerText = `Вы загадали число ${answerNumber }?`;
+answerField.innerText = `Вы загадали число ${answerNumber}?`;
 
 document.getElementById('btnRetry').addEventListener('click', function () {
     minValue = 0;
@@ -17,10 +17,10 @@ document.getElementById('btnRetry').addEventListener('click', function () {
     orderNumber = 0;
 })
 
-document.getElementById('btnOver').addEventListener('click', function () { // Код для кнопки «Больше»; происходит изменение нижней границы поискового диапазона.
+document.getElementById('btnOver').addEventListener('click', function () { // Код для кнопки «Больше».
     if (gameRun){
         if (minValue === maxValue){
-            const phraseRandom = Math.round( Math.random());
+            const phraseRandom = Math.round(Math.random());
             const answerPhrase = (phraseRandom === 1) ?
                 `Вы загадали неправильное число!\n\u{1F914}` :
                 `Я сдаюсь..\n\u{1F92F}`;
@@ -28,8 +28,8 @@ document.getElementById('btnOver').addEventListener('click', function () { // К
             answerField.innerText = answerPhrase;
             gameRun = false;
         } else {
-            minValue = answerNumber  + 1;
-            answerNumber  = Math.floor((minValue + maxValue) / 2);
+            minValue = answerNumber + 1; // Изменение нижней границы поискового диапазона.
+            answerNumber = Math.floor((minValue + maxValue) / 2);
             orderNumber++;
             orderNumberField.innerText = orderNumber;
             answerField.innerText = `Вы загадали число ${answerNumber }?`;
@@ -37,10 +37,10 @@ document.getElementById('btnOver').addEventListener('click', function () { // К
     }
 })
 
-document.getElementById('btnLess').addEventListener('click', function () { // Код для кнопки «Меньше»; происходит изменение верхней границы поискового диапазона.
+document.getElementById('btnLess').addEventListener('click', function () { // Код для кнопки «Меньше».
     if (gameRun){
         if (minValue === maxValue){
-            const phraseRandom = Math.round( Math.random());
+            const phraseRandom = Math.round(Math.random());
             const answerPhrase = (phraseRandom === 1) ?
                 `Вы загадали неправильное число!\n\u{1F914}` :
                 `Я сдаюсь..\n\u{1F92F}`;
@@ -48,8 +48,8 @@ document.getElementById('btnLess').addEventListener('click', function () { // К
             answerField.innerText = answerPhrase;
             gameRun = false;
         } else {
-            maxValue = answerNumber - 1;
-            answerNumber  = Math.floor((minValue + maxValue) / 2);
+            maxValue = answerNumber - 1; // Изменение верхней границы поискового диапазона.
+            answerNumber = Math.floor((minValue + maxValue) / 2);
             orderNumber++;
             orderNumberField.innerText = orderNumber;
             answerField.innerText = `Вы загадали число ${answerNumber }?`;
