3158a85ac4a05f799fc7a3e1f247099b72665f0a
alexeyvsokolov <alexeysokolovgeneral@gmail.com>
2022-06-06 16:33:54 +0300

    fix code for max and min number value

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
 let maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
 
-minValue = (minValue < -999) ? minValue = -999 : (minValue > 999) ? minValue = 999 : maxValue;
-maxValue = (maxValue > 999) ? maxValue = 999 : (maxValue > 999) ? maxValue = 999 : maxValue;
+minValue = (minValue < -999) ? minValue = -999 : (minValue > 999) ? minValue = 999 : minValue;
+maxValue = (maxValue > 999) ? maxValue = 999 : (maxValue < -999) ? maxValue = -999 : maxValue;
+
+if (maxValue < minValue) {
+    [maxValue, minValue] = [minValue, maxValue]; // Значения меняются местами если max меньше min.
+}
 
 alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
 let answerNumber = Math.floor((minValue + maxValue) / 2); // Середина числового диапазона
@@ -18,8 +22,8 @@ answerField.innerText = `Вы загадали число ${answerNumber}?`; // 
 document.getElementById('btnRetry').addEventListener('click', function () { // Кнопка "Заново"
     minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
     maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
-    minValue = (minValue < -999) ? minValue = -999 : (minValue > 999) ? minValue = 999 : maxValue;
-    maxValue = (maxValue > 999) ? maxValue = 999 : (maxValue > 999) ? maxValue = 999 : maxValue;
+    minValue = (minValue < -999) ? minValue = -999 : (minValue > 999) ? minValue = 999 : minValue;
+    maxValue = (maxValue > 999) ? maxValue = 999 : (maxValue < -999) ? maxValue = -999 : maxValue;
     alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
     answerNumber = Math.floor((minValue + maxValue) / 2); // Середина числового диапазона
     orderNumber = 1; // Номер первого вопроса.
