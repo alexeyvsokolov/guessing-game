4b870302f2ecd777962735085d99dd821a005fcf
alexeyvsokolov <alexeysokolovgeneral@gmail.com>
2022-09-29 04:31:44 +0300

    update the function of the button leading to the game

--- script.js
+++ script.js
@@ -40,69 +40,200 @@ document.getElementById('btnPlay').addEventListener('click', function () {      
     document.querySelector('#btnEqual').classList.remove('hidden');             // Кнопка Верно! / класс hidden удаляется; кнопка появляется
     document.querySelector('#btnOver').classList.remove('hidden');              // Кнопка больше / класс hidden удаляется; кнопка появляется
     document.querySelector('.btn-link').classList.remove('hidden');             // Кнопка Заново / класс hidden удаляется; кнопка появляется
+
+    let answerNumber = Math.floor((minValue + maxValue) / 2); // Середина числового диапазона
+    let orderNumber = 1; // Номер первого вопроса.
+    let gameRun = true;
+
+    const orderNumberField = document.getElementById('orderNumberField'); // Вопрос №_
+    const answerField = document.getElementById('answerField');
+
+    // Преобразования числа в текстовую форму. Число выводится в текстовой форме, если в текстовой форме меньше 20 символов, включая пробелы.
+
+    let units = ['', 'один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять'];
+    let teens = ['', 'десять', 'одинадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать'];
+    let dozens = ['', 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто'];
+    let hundreds = ['', 'сто', 'двести', 'триста', 'четыреста', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот'];
+
+    function numberToText() { // Функция преобразования числа из цифр в слова (числа от -999 до 999).
+        let number = Math.abs(answerNumber);
+        let text = '';
+
+        if (number == 0) {
+            text = 'ноль';
+            return text;
+        }
+
+        if (number <= 9) {
+            return units[Math.floor(Math.abs(number) / 1)];
+        }
+
+        if (number > 9 && number < 20) {
+            return teens[Math.floor(number / 10 + number % 10)];
+        }
+
+        if (number >= 20 && number <= 99) {
+            return dozens[(Math.floor(number / 10)) - 1] + " " + units[Math.floor(number % 10)];
+        }
+
+        if (number >= 100 && number <= 999) {
+            return hundreds[Math.floor(number / 100)] + " " + numberToTextHundreds();
+        }
+    }
+
+    function numberToTextHundreds() { // Функция вычисления остатка от сотого числа и преобразования его в числа из цифр в слова (числа от 0 до 99) для последующего присоединения к функции numberToText() расчитывающей сотни hundreds.
+        let unitsTeensDozens = Math.abs(answerNumber) % 100;
+
+        if (unitsTeensDozens <= 9) {
+            return units[Math.floor(unitsTeensDozens / 1)];
+        }
+
+        if (unitsTeensDozens > 9 && unitsTeensDozens < 20) {
+            return teens[(Math.floor(unitsTeensDozens / 10)) + (unitsTeensDozens % 10)];
+        }
+
+        if (unitsTeensDozens >= 20 && unitsTeensDozens <= 99) {
+            return dozens[(Math.floor(unitsTeensDozens / 10)) - 1] + " " + units[Math.floor(unitsTeensDozens % 10)];
+        }
+    }
+
+    orderNumberField.innerText = orderNumber; // Вопрос № 1
+    answerField.innerText = answerNumber >= 0 ? numberToText().length < 20 && answerNumber >= 0 ? `Вы загадали число ${numberToText()}?` : `Вы загадали число ${answerNumber}?` : numberToText().length < 20 ? `Вы загадали число минус ${numberToText()}?` : `Вы загадали число ${answerNumber}?`;
+    // Вы загадали число [__]? - Середина числового диапазона (answerNumber - подставляется середина числового диапазона в функцию numberToText() которая преобразует в текст и записывается в переменную numberWord).
+
+    document.getElementById('btnLess').addEventListener('click', function () { // Код для кнопки «Меньше».
+        if (gameRun) {
+            if (minValue === maxValue || minValue == answerNumber) {
+                const phraseRandom = Math.round(Math.random() * 3);
+                switch (phraseRandom) {
+                    case 0:
+                        answerPhrase = `Вы загадали неправильное число!\n\u{1F914}`
+                        break;
+
+                    case 1:
+                        answerPhrase = `Вы забыли, какое число загадали?\n\u{1F92A}`
+                        break;
+
+                    case 2:
+                        answerPhrase = `Вы ошиблись с числом!\n\u{1F9D0}`
+                        break;
+
+                    case 3:
+                        answerPhrase = `Не жульничайте!\n\u{1F620}`
+                        break;
+                }
+                answerField.innerText = answerPhrase;
+                gameRun = false;
+            } else {
+                maxValue = answerNumber - 1; // Изменение верхней границы поискового диапазона.
+                answerNumber = Math.floor((minValue + maxValue) / 2);
+                orderNumber++;
+                orderNumberField.innerText = orderNumber;
+                const phraseRandom = Math.round(Math.random() * 4); // Генерируется случайное число от 0 до 4.
+                switch (phraseRandom) {
+                    case 1:
+                        answerPhrase = `Наверное, это число `
+                        break;
+
+                    case 2:
+                        answerPhrase = `Возможно `
+                        break;
+
+                    case 3:
+                        answerPhrase = `Это число `
+                        break;
+
+                    case 4:
+                        answerPhrase = `Скорее всего это число `
+                        break;
+                }
+                answerField.innerText = answerNumber >= 0 ? numberToText().length < 20 && answerNumber >= 0 ? `Вы загадали число ${numberToText()}?` : `Вы загадали число ${answerNumber}?` : numberToText().length < 20 ? `Вы загадали число минус ${numberToText()}?` : `Вы загадали число ${answerNumber}?`;
+            }
+        }
+    })
+
+    document.getElementById('btnOver').addEventListener('click', function () { // Код для кнопки «Больше».
+        if (gameRun) {
+            if (minValue === maxValue) {
+                const phraseRandom = Math.round(Math.random() * 3);
+                switch (phraseRandom) {
+                    case 0:
+                        answerPhrase = `Вы загадали неправильное число!\n\u{1F914}`
+                        break;
+
+                    case 1:
+                        answerPhrase = `Вы забыли, какое число загадали?\n\u{1F92A}`
+                        break;
+
+                    case 2:
+                        answerPhrase = `Вы ошиблись с числом!\n\u{1F9D0}`
+                        break;
+
+                    case 3:
+                        answerPhrase = `Не жульничайте!\n\u{1F620}`
+                        break;
+                }
+                answerField.innerText = answerPhrase;
+                gameRun = false;
+            } else {
+                minValue = answerNumber + 1; // Изменение нижней границы поискового диапазона.
+                answerNumber = Math.floor((minValue + maxValue) / 2);
+                orderNumber++;
+                orderNumberField.innerText = orderNumber;
+                const phraseRandom = Math.round(Math.random() * 4); // Генерируется случайное число от 0 до 4.
+                switch (phraseRandom) {
+                    case 0:
+                        answerPhrase = `Вы загадали число `
+                        break;
+
+                    case 1:
+                        answerPhrase = `Наверное, это число `
+                        break;
+
+                    case 2:
+                        answerPhrase = `Возможно `
+                        break;
+
+                    case 3:
+                        answerPhrase = `Это число `
+                        break;
+
+                    case 4:
+                        answerPhrase = `Скорее всего это число `
+                        break;
+                }
+                answerField.innerText = answerNumber >= 0 ? numberToText().length < 20 && answerNumber >= 0 ? `Вы загадали число ${numberToText()}?` : `Вы загадали число ${answerNumber}?` : numberToText().length < 20 ? `Вы загадали число минус ${numberToText()}?` : `Вы загадали число ${answerNumber}?`;
+            }
+        }
+    })
+
+    document.getElementById('btnEqual').addEventListener('click', function () { // Код для кнопки "Верно".
+        if (gameRun) {
+            const phraseRandom = Math.round(Math.random() * 3);
+            switch (phraseRandom) {
+                case 0:
+                    answerPhrase = `Я всегда угадываю\n\u{1F60E}`
+                    break;
+
+                case 1:
+                    answerPhrase = `Yes! \n\u{1F60E}`
+                    break;
+
+                case 2:
+                    answerPhrase = `Отлично!\n\u{1F973}`
+                    break;
+
+                case 3:
+                    answerPhrase = `Я выиграл!\n\u{1F929}`
+                    break;
+            }
+            answerField.innerText = answerPhrase;
+            gameRun = false;
+        }
+    })
 })
 
-let answerNumber = Math.floor((minValue + maxValue) / 2); // Середина числового диапазона
-let orderNumber = 1; // Номер первого вопроса.
-let gameRun = true;
-
-const orderNumberField = document.getElementById('orderNumberField'); // Вопрос №_
-const answerField = document.getElementById('answerField');
-
-// Преобразования числа в текстовую форму. Число выводится в текстовой форме, если в текстовой форме меньше 20 символов, включая пробелы.
-
-let units = ['', 'один', 'два', 'три', 'четыре', 'пять', 'шесть','семь', 'восемь', 'девять'];
-let teens = ['', 'десять', 'одинадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать','шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать'];
-let dozens = ['', 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят','восемьдесят', 'девяносто'];
-let hundreds = ['', 'сто', 'двести', 'триста', 'четыреста', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот'];
-
-function numberToText() { // Функция преобразования числа из цифр в слова (числа от -999 до 999).
-    let number = Math.abs(answerNumber);
-    let text = '';
-
-    if (number == 0) {
-        text = 'ноль';
-        return text;
-    }
-
-    if (number <= 9) {
-        return units[Math.floor(Math.abs(number) / 1)];
-    }
-
-    if (number > 9 && number < 20) {
-        return teens[Math.floor(number / 10 + number % 10)];
-    }
-
-    if (number >= 20 && number <= 99) {
-        return dozens[(Math.floor(number / 10)) - 1] + " " + units[Math.floor(number % 10)];
-    }
-
-    if (number >= 100 && number <= 999) {
-        return hundreds[Math.floor(number / 100)] + " " + numberToTextHundreds();
-    }
-}
-
-function numberToTextHundreds() { // Функция вычисления остатка от сотого числа и преобразования его в числа из цифр в слова (числа от 0 до 99) для последующего присоединения к функции numberToText() расчитывающей сотни hundreds.
-    let unitsTeensDozens = Math.abs(answerNumber) % 100;
-    
-    if (unitsTeensDozens <= 9) {
-        return units[Math.floor(unitsTeensDozens / 1)];
-    }
-
-    if (unitsTeensDozens > 9 && unitsTeensDozens < 20) {
-        return teens[(Math.floor(unitsTeensDozens / 10)) + (unitsTeensDozens % 10)];
-    }
-
-    if (unitsTeensDozens >= 20 && unitsTeensDozens <= 99) {
-        return dozens[(Math.floor(unitsTeensDozens / 10)) - 1] + " " + units[Math.floor(unitsTeensDozens % 10)];
-    }
-}
-
-orderNumberField.innerText = orderNumber; // Вопрос № 1
-answerField.innerText = answerNumber >= 0 ? numberToText().length < 20 && answerNumber >= 0 ? `Вы загадали число ${numberToText()}?` : `Вы загадали число ${answerNumber}?`: numberToText().length < 20 ? `Вы загадали число минус ${numberToText()}?` : `Вы загадали число ${answerNumber}?`;
-// Вы загадали число [__]? - Середина числового диапазона (answerNumber - подставляется середина числового диапазона в функцию numberToText() которая преобразует в текст и записывается в переменную numberWord).
-
-document.getElementById('btnRetry').addEventListener('click', function () { // Кнопка "Заново"
+/* document.getElementById('btnRetry').addEventListener('click', function () { // Кнопка "Заново"
     minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
     maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
     minValue = (minValue < -999) ? minValue = -999 : (minValue > 999) ? minValue = 999 : minValue;
@@ -120,135 +251,4 @@ document.getElementById('btnRetry').addEventListener('click', function () { // �
     orderNumberField.innerText = orderNumber; // Вопрос № 1
     answerField.innerText = answerNumber >= 0 ? numberToText().length < 20 && answerNumber >= 0 ? `Вы загадали число ${numberToText()}?` : `Вы загадали число ${answerNumber}?`: numberToText().length < 20 ? `Вы загадали число минус ${numberToText()}?` : `Вы загадали число ${answerNumber}?`;
     gameRun = true;
-})
-
-document.getElementById('btnLess').addEventListener('click', function () { // Код для кнопки «Меньше».
-    if (gameRun){
-        if (minValue === maxValue || minValue == answerNumber){
-            const phraseRandom = Math.round(Math.random() * 3);
-            switch (phraseRandom) {
-                case 0: 
-                    answerPhrase = `Вы загадали неправильное число!\n\u{1F914}`
-                    break;      
-            
-                case 1:
-                    answerPhrase = `Вы забыли, какое число загадали?\n\u{1F92A}`
-                    break;
-
-                case 2: 
-                    answerPhrase = `Вы ошиблись с числом!\n\u{1F9D0}`
-                    break;      
-            
-                case 3:
-                    answerPhrase = `Не жульничайте!\n\u{1F620}`
-                    break;
-            }
-            answerField.innerText = answerPhrase;
-            gameRun = false;
-        } else {
-            maxValue = answerNumber - 1; // Изменение верхней границы поискового диапазона.
-            answerNumber = Math.floor((minValue + maxValue) / 2);
-            orderNumber++;
-            orderNumberField.innerText = orderNumber;
-            const phraseRandom = Math.round(Math.random() * 4); // Генерируется случайное число от 0 до 4.
-            switch (phraseRandom) {
-                case 1:
-                    answerPhrase = `Наверное, это число `
-                    break;
-
-                case 2: 
-                    answerPhrase = `Возможно `
-                    break;      
-            
-                case 3:
-                    answerPhrase = `Это число `
-                    break;
-
-                case 4:
-                    answerPhrase = `Скорее всего это число `
-                    break;
-            }            
-            answerField.innerText = answerNumber >= 0 ? numberToText().length < 20 && answerNumber >= 0 ? `Вы загадали число ${numberToText()}?` : `Вы загадали число ${answerNumber}?`: numberToText().length < 20 ? `Вы загадали число минус ${numberToText()}?` : `Вы загадали число ${answerNumber}?`;
-        }
-    }
-})
-
-document.getElementById('btnOver').addEventListener('click', function () { // Код для кнопки «Больше».
-    if (gameRun){
-        if (minValue === maxValue){
-            const phraseRandom = Math.round(Math.random() * 3);
-            switch (phraseRandom) {
-                case 0: 
-                    answerPhrase = `Вы загадали неправильное число!\n\u{1F914}`
-                    break;      
-            
-                case 1:
-                    answerPhrase = `Вы забыли, какое число загадали?\n\u{1F92A}`
-                    break;
-
-                case 2: 
-                    answerPhrase = `Вы ошиблись с числом!\n\u{1F9D0}`
-                    break;      
-            
-                case 3:
-                    answerPhrase = `Не жульничайте!\n\u{1F620}`
-                    break;
-            }
-            answerField.innerText = answerPhrase;
-            gameRun = false;
-        } else {
-            minValue = answerNumber + 1; // Изменение нижней границы поискового диапазона.
-            answerNumber = Math.floor((minValue + maxValue) / 2);
-            orderNumber++;
-            orderNumberField.innerText = orderNumber;
-            const phraseRandom = Math.round(Math.random() * 4); // Генерируется случайное число от 0 до 4.
-            switch (phraseRandom) {
-                case 0: 
-                    answerPhrase = `Вы загадали число `
-                    break;      
-            
-                case 1:
-                    answerPhrase = `Наверное, это число `
-                    break;
-
-                case 2: 
-                    answerPhrase = `Возможно `
-                    break;      
-            
-                case 3:
-                    answerPhrase = `Это число `
-                    break;
-
-                case 4:
-                    answerPhrase = `Скорее всего это число `
-                    break;
-            }
-            answerField.innerText = answerNumber >= 0 ? numberToText().length < 20 && answerNumber >= 0 ? `Вы загадали число ${numberToText()}?` : `Вы загадали число ${answerNumber}?`: numberToText().length < 20 ? `Вы загадали число минус ${numberToText()}?` : `Вы загадали число ${answerNumber}?`;
-        }
-    }
-})
-
-document.getElementById('btnEqual').addEventListener('click', function () { // Код для кнопки "Верно".
-    if (gameRun){
-        const phraseRandom = Math.round(Math.random() * 3);
-            switch (phraseRandom) {
-                case 0: 
-                    answerPhrase = `Я всегда угадываю\n\u{1F60E}`
-                    break;      
-            
-                case 1:
-                    answerPhrase = `Yes! \n\u{1F60E}`
-                    break;
-
-                case 2: 
-                    answerPhrase = `Отлично!\n\u{1F973}`
-                    break;      
-            
-                case 3:
-                    answerPhrase = `Я выиграл!\n\u{1F929}`
-                    break;
-            }
-        answerField.innerText = answerPhrase;
-        gameRun = false;
-    }
-})
+}) */
